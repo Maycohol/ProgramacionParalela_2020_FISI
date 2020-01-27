a5c640eb72c3f5554331b61a38f08253cfd1ee1f
Maycohol <54748451+Maycohol@users.noreply.github.com>
2020-01-27 02:46:10 -0500

    Add files via upload

---
+++ Laboratorio_04/prog05.1_Trapezoid/Debug/src/mpi_trap1.d
@@ -0,0 +1,7 @@
+src/mpi_trap1.o: ../src/mpi_trap1.c \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:

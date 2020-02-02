c640724efdbbb406809456a85de50a8d6290a766
Maycohol <54748451+Maycohol@users.noreply.github.com>
2020-02-02 01:33:26 -0500

    Add files via upload

---
+++ Tiempo_Transcurrido_Secuencial/Debug/src/Tiempo_Transcurrido_Secuencial.d
@@ -0,0 +1,8 @@
+src/Tiempo_Transcurrido_Secuencial.o: \
+ ../src/Tiempo_Transcurrido_Secuencial.c \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:

42c4e35168c49dec579ff15612acfa8e551bc974
Maycohol <54748451+Maycohol@users.noreply.github.com>
2020-02-27 14:22:34 -0500

    Add files via upload

---
+++ Laboratorio_13/Lab13-Directiva-For-Private/src/principal-master.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : Lab13-Directiva-Critical.c
+ Author      : Maycohol Jimmy Ayra Herrera
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Directiva critical
+ ============================================================================
+ */
+// omp_master.cpp
+// compile with: /openmp
+#include <omp.h>
+#include <stdio.h>
+
+int main( )
+{
+    int a[5], i;
+
+    #pragma omp parallel
+    {
+        // Perform some computation.
+        #pragma omp for
+        for (i = 0; i < 5; i++)
+            a[i] = i * i;
+
+        // Print intermediate results.
+        #pragma omp master
+            for (i = 0; i < 5; i++)
+                printf("a[%d] = %d\n", i, a[i]);
+
+        // Wait.
+        #pragma omp barrier
+
+        // Continue with the computation.
+        #pragma omp for
+        for (i = 0; i < 5; i++)
+            a[i] += i;
+    }
+}
